03133b43144300663e45ee6941eb7837388595e7
xuan13hao <xuan13hao@gmail.com>
2026-06-18 21:23:49 -0500

    Clean up run_bash2.sh by removing comments
    Removed comments and redundant lines from the script.

--- run_bash2.sh
+++ run_bash2.sh
@@ -8,12 +8,4 @@ nextflow run /home/h392x566/rsVAT \
   --max_cpus 32 --max_memory '120.GB'\
   --multiqc_title "Mouse_TotalRNA_PE1011_VAT" \
   --vat_single_end_bam true \
-  -profile singularity \
-  -resume
-
-# The run you asked for:
-# bash run_bash2.sh          # uses --aligner vat; vat_single_end_bam defaults to true
-
-# To disable the behavior (e.g. compare/confirm the old failure):
-# nextflow run /home/h392x566/rsVAT --input sample1.csv --aligner vat \
-#   --vat_single_end_bam false -profile singularity -resume
+  -profile singularity 
